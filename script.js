8a3d158a701df6da135f6c854255f991c0a85801
shweta <shwetakale2207@gmail.com>
2023-06-20 11:08:58 +0530

    created navbar, content and footer using create element

--- script.js
+++ script.js
@@ -7,19 +7,19 @@
 //{}=>this indicates properties of the element 
 
 //adding property 
-const Reactelement = React.createElement('div', {},
-    React.createElement('h1', {}, 'Hello World!'),
-    React.createElement('ul', {},
-        React.createElement('li', {}, "One"), //content
-        React.createElement('li', {}, "Two"),//content
-        React.createElement('li', {}, "Three"),//content
-    )
-)
+// const Reactelement = React.createElement('div', {},
+//     React.createElement('h1', {}, 'Hello World!'),
+//     React.createElement('ul', {},
+//         React.createElement('li', {}, "One"), //content
+//         React.createElement('li', {}, "Two"),//content
+//         React.createElement('li', {}, "Three"),//content
+//     )
+// )
 
 
 
-const nativeElement = document.createElement('h1');
-nativeElement.innerHTML = "helllo shwet";
+// const nativeElement = document.createElement('h1');
+// nativeElement.innerHTML = "helllo shwet";
 
 // console.dir(nativeElement)
 // console.log(Reactelement);
@@ -28,6 +28,33 @@ nativeElement.innerHTML = "helllo shwet";
 // document.body.appendChild(Reactelement) // gives error as append child only works on a node and this is a react element  
 
 //now adding the ReactElement on the Dom
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Reactelement)
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(Reactelement)
 //Root can have only one child in react
+
+
+// ------------------------------------------------------------------------------------------
+
+//creating a navbar
+
+
+const Navbar =
+    React.createElement("nav", {},
+
+        React.createElement('ul', {},
+            React.createElement("li", {
+                onClick: () => console.log("This is a Home Page"),
+                className: "nav-link"
+            }, "Home"),
+            React.createElement("li", {}, "About"),
+            React.createElement("li", {}, "Contact"),
+        )
+    );
+
+
+const Content = React.createElement('div', {}, 'content');
+const Footer = React.createElement('div', {}, 'footer');
+
+const App = React.createElement('div', {}, Navbar, Content, Footer)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(App)
